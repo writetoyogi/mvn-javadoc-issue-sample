ede54d7f25bd6ca7dc2b2d7bbf13fb45e3ecb879
writetoyogi <ybdesai111@gmail.com>
2022-09-26 11:29:53 +0530

    Initial Commit

---
+++ src/main/java/SampleJavaDoc.java
@@ -0,0 +1,14 @@
+
+public class SampleJavaDoc {
+	
+	
+	/**
+	 * This method is created for demonstrating sample
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//TO-DO
+		
+	}
+
+}
